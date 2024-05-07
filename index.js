cec06125a809097051783f084e0911452ae0cefa
Abdullah-git-hub <arafat200423@gmail.com>
2024-05-07 15:57:00 +0600

    server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+app.listen(3000);
+
+app.get("/", (req, res) => {
+    res.sendFile("./views/index.html", { root: __dirname });
+});
+
+app.get("/uni/:country_name", (req, res) => {
+    const country = req.params.country_name.split("+").join(" ");
+    fs.readFile(`./countries/${country}.json`, (err, data) => {
+        if (err) {
+            res.status(404).send("<h1>Country not found</h1>");
+        } else {
+            const uni_list = JSON.parse(data.toString());
+            res.json({
+                uni_list,
+            });
+        }
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).send("<h1>Webpage not found</h1>");
+});
